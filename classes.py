7e40a8363ad768e06fb9eaa3c79d3008fe3aa4b8
Roberto Ferraris <ro.ferraris@gmail.com>
2019-10-21 22:21:35 +0200

    Added class method

--- classes.py
+++ classes.py
@@ -1,10 +1,10 @@
+students = []
 class Student:
-    pass
+    def add_student(self, name, student_id=None):
+        student = {"name": name, "student_id": student_id}
+        students.append(student)
 
 student = Student()
+student.add_student("Mark")
 
-print(student)
-
-new_student = Student()
-
-print(new_student)
+print(students)
